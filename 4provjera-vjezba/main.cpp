6d7e13b74548fbdcd82c56242cc0e2d9069900d6
Leo-Klisanic <64528179+Leo-Klisanic@users.noreply.github.com>
2020-05-03 15:03:02 +0200

    2. funkcija dodana
    Dodana funkcija ispisa svih racuna i funkcija najvecih racuna ...

--- 4provjera-vjezba/main.cpp
+++ 4provjera-vjezba/main.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 #include<windows.h>
 #include<conio.h>
+#include<algorithm>
+#include<numeric>
 using namespace std;
+
+#define test cout<<"\nTu sam";
 
 void main_menu(){
     cout<<"1. Unos novog racuna\n";
@@ -31,7 +35,33 @@ void novi_racun(unsigned long long br_racuna[1000], string ime[1000], double sal
     cin>>ime[korisnik];
     cout<<"Unesite saldo: ";
     cin>>saldo[korisnik];
-    korisnik++;
+}
+
+bool negativan(double br){
+    if(br<0){
+        return true;
+    }
+    return false;
+}
+
+bool tekuci(unsigned long long br){
+    if(br/100000000==32){
+        return true;
+    }
+    return false;
+}
+
+void ispis_podataka(unsigned long long br_racuna[], string ime[], double saldo[], int korisnik, double &s, string &bist, int &brNegativnih, int &brTekucih){
+    for(int i=0;i<korisnik;i++){
+        cout<<"Broj racuna: "<<br_racuna[i]<<"\n";
+        cout<<"Ime i prezime: "<<ime[i]<<"\n";
+        cout<<"Saldo: "<<saldo[i]<<"\n\n";
+    }
+    s=accumulate(saldo, saldo+korisnik, 0);
+    int maxi=max_element(saldo, saldo+korisnik)-saldo;
+    bist=ime[maxi];
+    brNegativnih=count_if(br_racuna, br_racuna+korisnik, negativan);
+    brTekucih=count_if(br_racuna, br_racuna+korisnik, tekuci);
 }
 
 int main()
@@ -47,9 +77,22 @@ int main()
         cin>>choice;
         if(choice==1){
             novi_racun(br_racuna, ime, saldo, korisnik);
+            korisnik++;
         }
         else if(choice==2){
-
+            if(korisnik>0){
+                double s;
+                string bist;
+                int brNegativnih;
+                int brTekucih;
+                ispis_podataka(br_racuna, ime, saldo, korisnik, s, bist, brNegativnih, brTekucih);
+                cout<<"Zbroj novaca: "<<s<<endl<<"Racun sa najvise novaca: "<<bist<<endl<<"Negativni racuni: "<<brNegativnih<<endl<<"Broj tekucih racuna: "<<brTekucih<<endl;
+                getch();
+            }
+            else{
+                cout<<"Nema racuna koje mozemo prikazati"<<"\n";
+            }
+            getch();
         }
         else if(choice==3){
 
